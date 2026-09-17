0c76bdfc87f787e549d5582ec744d6200ac57cc3
KevinAGCRRT <00026825@uca.edu.sv>
2026-09-16 21:09:48 -0600

    fix: eliminar errror de ubicacion app.js

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const incidenciasRoutes = require('./routes/incidencias');
 const app = express();
 const PORT = 3000;
+app.use(express
+const express = require('express');
+const incidenciasRoutes = require('./routes/incidencias');
+const app = express();
+const PORT = 3000;
 app.use(express.json());
 app.use('/', incidenciasRoutes);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});.json());
+app.use('/', incidenciasRoutes);
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
